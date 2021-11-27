8df680fb20bb7b201447e1b61c9312a4171e529d
TheSlavicFox <warren.troyer@outlook.com>
2021-11-27 05:39:59 -0500

    Uhhhh
    test

---
+++ levelsys.py
@@ -0,0 +1,6 @@
+import discord
+from discord.ext import commands
+from pymongo import MongoClient
+
+bot_channel = 872904558878265355
+talk_channels = [872904558626627631]
---
+++ main.py
@@ -0,0 +1,13 @@
+from discord.ext import commands
+import discord
+import levelsys
+
+cogs = [levelsys]
+
+client = commands.Bot(command_prefix="?", intents=discord.Intents.all())
+
+for i in range(len(cogs)):
+    ##set up cogs
+    print("uhhh")
+
+client.run(OTE0MDk3MjM2ODE0NzM3NDE4.YaIFZA.EgufVbdJtBrNKUUvc4SYJPqci-8)
